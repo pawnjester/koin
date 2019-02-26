69167d34499588f8584903a2a86f342b3ee6b8a8
okonji emmanuel <ebukeokonji@gmail.com>
2019-02-26 21:37:39 +0100

    edit-koin

--- app/src/main/java/com/andela/learnkoin/MainActivity.kt
+++ app/src/main/java/com/andela/learnkoin/MainActivity.kt
@@ -2,11 +2,15 @@ package com.andela.learnkoin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.andela.Presenter
+import org.koin.android.ext.android.inject
 import org.koin.core.Koin.Companion.logger
 import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.startKoin
 
 class MainActivity : AppCompatActivity() {
+
+    val presenter: Presenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/andela/learnkoin/Module.kt
+++ app/src/main/java/com/andela/learnkoin/Module.kt
@@ -4,5 +4,6 @@ import com.andela.Presenter
 import org.koin.dsl.module.module
 
 val appModule = module {
+    single<Repository> { RepositoryImpl() }
     factory { Presenter(get()) }
 }
